c7b8a1701a26e995b646d231d56adccbdeb75948
Ogwarf <ome504@hotmail.com>
2021-04-21 13:27:12 +0700

    :hammer: +HomeScreen

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -4,8 +4,15 @@ import { createStackNavigator } from "react-navigation-stack";
 
 import Home from "../screen/HomeScreen";
 
-const MyNavigator = createStackNavigator({
-  H: Home,
-});
+const MyNavigator = createStackNavigator(
+  {
+    H: Home,
+  },
+  {
+    defaultNavigationOptions: {
+      headerShown: false,
+    },
+  }
+);
 
 export default createAppContainer(MyNavigator);
