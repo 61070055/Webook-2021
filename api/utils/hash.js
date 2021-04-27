76498a3e73c7526c7af6349033901200221d0939
Tetat Chulchue <tetat.chulchue@gmail.com>
2021-04-27 11:31:18 +0700

    🔨 Add script for hashing password

---
+++ api/utils/hash.js
@@ -0,0 +1,15 @@
+const crypto = require('crypto')
+
+function genRandomString(length) {
+    return crypto.randomBytes(Math.ceil(length/2))
+            .toString('hex')
+            .slice(0,length)
+}
+
+function hashPassword(password, salt) {
+    let paaswordToBeHash = password.toString().concat(salt)
+    return crypto.createHash('sha512').update(paaswordToBeHash).digest('hex');
+}
+
+module.exports.hashPassword = hashPassword
+module.exports.genRandomString = genRandomString
